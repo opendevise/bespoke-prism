f4ca682f89c1cc3861820f732e1e1db8b8d4c6b0
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-06-17 18:43:18 +0200

    feat: init plugin with PrismJS theme and plugins
    Plugins :
    
    * prism-unescaped-markup to simplify HTML code examples
    * prism-normalize-whitespace to avoid silly indentation in HTML slides

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+'use strict';
+var pkg = require('./package.json'),
+  browserify = require('browserify'),
+  stringify = require('stringify'),
+  buffer = require('vinyl-buffer'),
+  concat = require('gulp-concat'),
+  del = require('del'),
+  gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  header = require('gulp-header'),
+  jshint = require('gulp-jshint'),
+  karma = require('karma'),
+  ghpages = require('gh-pages'),
+  path = require('path'),
+  rename = require('gulp-rename'),
+  replace = require('gulp-replace'),
+  source = require('vinyl-source-stream'),
+  uglify = require('gulp-uglify');
+
+gulp.task('default', ['clean', 'lint', 'test', 'compile']);
+gulp.task('dev', ['compile', 'lint', 'test', 'watch']);
+
+gulp.task('watch', function () {
+  gulp.watch('lib/**/*.js', ['test', 'lint', 'compile']);
+  gulp.watch('test/spec/**/*.js', ['test']);
+});
+
+gulp.task('clean', function () {
+  return del(['dist', 'test/coverage']);
+});
+
+gulp.task('lint', function () {
+  return gulp.src(['gulpfile.js', 'lib/**/*.js', 'specs/**/*.js'])
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', function (done) {
+  new karma.Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+gulp.task('compile', ['clean'], function () {
+  return browserify('lib/bespoke-prism.js', { standalone: 'bespoke.plugins.prism' })
+    .transform(stringify, {
+      appliesTo: { includeExtensions: ['.css'] },
+      global: true
+    })
+    .bundle()
+    .pipe(source('bespoke-prism.js'))
+    .pipe(buffer())
+    .pipe(header([
+      '/*!',
+      ' * <%= name %> v<%= version %>',
+      ' *',
+      ' * Copyright <%= new Date().getFullYear() %>, <%= author.name %>',
+      ' * This content is released under the <%= license %> license',
+      ' */\n\n'
+    ].join('\n'), pkg))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('bespoke-prism.min.js'))
+    .pipe(uglify())
+    .pipe(header([
+      '/*! <%= name %> v<%= version %> ',
+      '© <%= new Date().getFullYear() %> <%= author.name %>, ',
+      '<%= license %> License */\n'
+    ].join(''), pkg))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('compile:demo', ['compile:demo:css', 'compile:demo:html', 'compile:demo:js']);
+
+gulp.task('compile:demo:css', ['clean'], function() {
+  return gulp.src('demo/style.css')
+    .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('compile:demo:html', ['clean'], function() {
+  return gulp.src('demo/index.html')
+    .pipe(replace(/<\/article>[\s\S]*?<script src="demo.js"><\/script>/, '</article>\n<script src="build.js"></script>'))
+    .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('compile:demo:js', ['compile'], function() {
+  return gulp.src([
+    'node_modules/bespoke/dist/bespoke.js',
+    'node_modules/bespoke-classes/dist/bespoke-classes.js',
+    'node_modules/bespoke-nav/dist/bespoke-nav.js',
+    'dist/bespoke-prism.js',
+    'demo/demo.js'
+  ])
+    .pipe(concat('build.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('demo/dist'));
+});
+
+gulp.task('deploy', ['compile:demo'], function(done) {
+  ghpages.publish(path.join(__dirname, 'demo/dist'), { logger: gutil.log }, done);
+});
---
+++ test/spec/bespoke-highlightSpec.js
@@ -0,0 +1,78 @@
+Function.prototype.bind = Function.prototype.bind || require('function-bind');
+
+var bespoke = require('bespoke'),
+  prism = require('../../lib/bespoke-prism.js');
+
+describe('bespoke-prism', function() {
+  var deck,
+    createDeck = function (done) {
+      var parent = document.createElement('article'),
+        sectionOne = document.createElement('section'),
+        sectionTwo = document.createElement('section');
+
+      sectionOne.innerHTML = [
+        '<pre><code class="language-javascript">',
+        '  function foobar(text) {',
+        '    console.log(text):',
+        '  }',
+        '</code></pre>'
+      ].join('\n');
+
+      sectionTwo.innerHTML = [
+        '<script type="text/plain" class="lang-markup">',
+        '  <div class="foo">',
+        '    <!-- comments FTW!! -->',
+        '    <p class="message">this <strong>IS</strong> a message!<p>',
+        '  </div>',
+        '</script>'
+      ].join('\n');
+
+      parent.appendChild(sectionOne);
+      parent.appendChild(sectionTwo);
+      document.body.appendChild(parent);
+
+      deck = bespoke.from(parent, [
+        prism()
+      ]);
+
+      // give PrismJS a chance to highlight code
+      setTimeout(done, 200)
+    },
+    destroyDeck = function() {
+      deck.fire('destroy');
+      var parentNode = deck.parent.parentNode;
+      if (parentNode) parentNode.removeChild(deck.parent);
+      var inlineStyles = document.head.querySelectorAll('style');
+      for (var i = 0; i < inlineStyles.length; i++) {
+        document.head.removeChild(inlineStyles[i]);
+      }
+      deck = null;
+    };
+
+  beforeEach(createDeck);
+  afterEach(destroyDeck);
+
+  it('should inject PrismJS styles', function() {
+    var firstStyleElement = document.head.querySelector('style');
+    expect(firstStyleElement.innerHTML).toContain('GHColors theme');
+  });
+
+  it('should insert <span>s with PrismJS classes and normalize whitespaces', function() {
+    var section = deck.parent.querySelectorAll('section')[0];
+    expect(section.innerHTML).toBe([
+      '<pre class=" language-javascript"><code class=" language-javascript"><span class="token keyword">function</span> <span class="token function">foobar</span><span class="token punctuation">(</span>text<span class="token punctuation">)</span> <span class="token punctuation">{</span>',
+      '  console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span>text<span class="token punctuation">)</span><span class="token punctuation">:</span>',
+      '<span class="token punctuation">}</span></code></pre>'
+    ].join('\n'));
+  });
+
+  it('should handle unescaped HTML as <script type="text/plain" class="lang-markup">', function() {
+    var section = deck.parent.querySelectorAll('section')[1];
+    expect(section.innerHTML).toBe([
+      '<pre class=" language-markup"><code class=" language-markup"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation">=</span><span class="token punctuation">"</span>foo<span class="token punctuation">"</span></span><span class="token punctuation">&gt;</span></span>',
+      '  <span class="token comment" spellcheck="true">&lt;!-- comments FTW!! --&gt;</span>',
+      '  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>p</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation">=</span><span class="token punctuation">"</span>message<span class="token punctuation">"</span></span><span class="token punctuation">&gt;</span></span>this <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>strong</span><span class="token punctuation">&gt;</span></span>IS<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>strong</span><span class="token punctuation">&gt;</span></span> a message!<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>p</span><span class="token punctuation">&gt;</span></span>',
+      '<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span></code></pre>'
+    ].join('\n'));
+  });
+});
