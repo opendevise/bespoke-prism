9e94c9c4702a4208528613fc74ba6640d35ace2b
Dan Allen <dan.j.allen@gmail.com>
2016-07-11 01:19:14 -0600

    code review for pr/2
    - change author in package.json
    - lock karma to 0.13.18 and socket.io to 1.3.7 to avoid hang on Linux
    - update other module dependencies
    - move client dependencies to dependencies block in package.json
    - move requires to top of file
    - insert CSS outside of constructor (since it's a static operation)
    - add dist folder
    - update description in README
    - add description to package.json
    - fix code indentation in test suite

--- test/spec/bespoke-highlightSpec.js
+++ test/spec/bespoke-highlightSpec.js
@@ -3,67 +3,61 @@ Function.prototype.bind = Function.prototype.bind || require('function-bind');
 var bespoke = require('bespoke'),
   highlight = require('../../lib/bespoke-highlight.js');
 
-describe('bespoke-highlight', function () {
+describe('bespoke-highlight', function() {
+  var deck,
+    createDeck = function(done) {
+      var parent = document.createElement('article'),
+        sectionOne = document.createElement('section'),
+        sectionTwo = document.createElement('section');
 
-  var deck;
+      sectionOne.innerHTML = [
+          '<pre><code class="language-javascript">',
+          '  function foobar(text) {',
+          '    console.log(text):',
+          '  }',
+          '</code></pre>'
+        ].join('\n');
 
-  var createDeck = function (done) {
+      sectionTwo.innerHTML = [
+        '<script type="text/plain" class="lang-markup">',
+        '  <div class="foo">',
+        '    <!-- comments FTW!! -->',
+        '    <p class="message">this <strong>IS</strong> a message!<p>',
+        '  </div>',
+        '</script>'
+      ].join('\n');
 
-    var parent = document.createElement('article');
+      parent.appendChild(sectionOne);
+      parent.appendChild(sectionTwo);
+      document.body.appendChild(parent);
 
-    var sectionOne = document.createElement('section');
-    sectionOne.innerHTML = [
-      '<pre><code class="language-javascript">',
-      '  function foobar(text) {',
-      '    console.log(text):',
-      '  }',
-      '</code></pre>'
-    ].join('\n');
-    parent.appendChild(sectionOne);
+      deck = bespoke.from(parent, [
+        highlight()
+      ]);
 
-    var sectionTwo = document.createElement('section');
-    sectionTwo.innerHTML = [
-      '<script type="text/plain" class="lang-markup">',
-      '  <div class="foo">',
-      '    <!-- comments FTW!! -->',
-      '    <p class="message">this <strong>IS</strong> a message!<p>',
-      '  </div>',
-      '</script>'
-    ].join('\n');
-    parent.appendChild(sectionTwo);
-
-    document.body.appendChild(parent);
-
-    deck = bespoke.from(parent, [
-      highlight()
-    ]);
-
-    deck.slide(0);
-
-    // defer so PrismJS can highlight code
-    setTimeout(done, 200)
-  };
-  var destroyDeck = function () {
-    deck.fire('destroy');
-    var parentNode = deck.parent.parentNode;
-    if (parentNode) parentNode.removeChild(deck.parent);
-    var inlineStyles = document.head.querySelectorAll('style');
-    for (var i = 0; i < inlineStyles.lenght; i++) {
-      document.head.removeChild(inlineStyles[i]);
-    }
-    deck = null;
-  };
+      // give PrismJS a chance to highlight code
+      setTimeout(done, 200)
+    };
+    destroyDeck = function() {
+      deck.fire('destroy');
+      var parentNode = deck.parent.parentNode;
+      if (parentNode) parentNode.removeChild(deck.parent);
+      var inlineStyles = document.head.querySelectorAll('style');
+      for (var i = 0; i < inlineStyles.length; i++) {
+        document.head.removeChild(inlineStyles[i]);
+      }
+      deck = null;
+    };
 
   beforeEach(createDeck);
   afterEach(destroyDeck);
 
-  it('should inject PrismJS styles', function () {
+  it('should inject PrismJS styles', function() {
     var firstStyleElement = document.head.querySelector('style');
     expect(firstStyleElement.innerHTML).toContain('GHColors theme');
   });
 
-  it('should insert <span>s with PrismJS classes and normalize whitespaces', function () {
-
+  it('should insert <span>s with PrismJS classes and normalize whitespaces', function() {
     var section = deck.parent.querySelectorAll('section')[0];
     expect(section.innerHTML).toBe([
       '<pre class=" language-javascript"><code class=" language-javascript"><span class="token keyword">function</span> <span class="token function">foobar</span><span class="token punctuation">(</span>text<span class="token punctuation">)</span> <span class="token punctuation">{</span>',
@@ -72,8 +66,7 @@ describe('bespoke-highlight', function () {
     ].join('\n'));
   });
 
-  it('should handle unescaped HTML as <script type="text/plain" class="lang-markup">', function () {
-
+  it('should handle unescaped HTML as <script type="text/plain" class="lang-markup">', function() {
     var section = deck.parent.querySelectorAll('section')[1];
     expect(section.innerHTML).toBe([
       '<pre class=" language-markup"><code class=" language-markup"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation">=</span><span class="token punctuation">"</span>foo<span class="token punctuation">"</span></span><span class="token punctuation">&gt;</span></span>',
