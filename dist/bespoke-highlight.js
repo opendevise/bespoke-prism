cef2cdbce04e6b712a0ac1ef2e644c88ff7bcb68
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-07-12 11:36:33 +0200

    Release 1.0.0

--- dist/bespoke-highlight.js
+++ dist/bespoke-highlight.js
@@ -1,5 +1,5 @@
 /*!
- * bespoke-highlight v1.0.0-dev
+ * bespoke-highlight v1.0.0
  *
  * Copyright 2016, Hubert SABLONNIÈRE
  * This content is released under the MIT license
@@ -10,12 +10,14 @@ var insertCss = require('insert-css'),
   ghColorsCss = require('prism-themes/themes/prism-ghcolors.css');
 
 module.exports = function() {
+  // prismjs directly tries to highlight source code examples once you require it
+  // that's why we cannot require it at module top level
   var Prism = require('prismjs');
   require('prismjs/plugins/unescaped-markup/prism-unescaped-markup');
   require('prismjs/plugins/normalize-whitespace/prism-normalize-whitespace');
   insertCss(ghColorsCss, { prepend: true });
 
-  return function () {
+  return function() {
     Prism.highlightAll();
   };
 };
